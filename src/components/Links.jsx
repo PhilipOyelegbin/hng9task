ee4e5f6ed7f1932dfdf2bb880cf9f8896d0e4a3a
Philip Oyelegbin <philipoyelegbin@gmail.com>
2022-10-28 08:40:05 +0100

    background link updated

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -4,7 +4,7 @@ const Links = () => {
     {id: "btn__zuri", label: "Zuri Team", links: "https://training.zuri.team/"},
     {id: "books", label: "Zuri Books", links: "http://books.zuri.team"},
     {id: "book__python", label: "Python Books", links: "https://books.zuri.team/python-for-beginners?ref_id=PhilipOyelegbin"},
-    {id: "pitch", label: "Background Check for Coders", links: "https://background.zuri.team"},
+    {id: "pitch", label: "Background Check for Coders", links: "https://backgroundcheck.zuri.team"},
     {id: "book__design", label: "Design Books", links: "https://books.zuri.team/design-rules"}
   ]
   return (
